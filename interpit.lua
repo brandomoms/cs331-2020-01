3f6a1779e7e8204ffad37078c22db8a3decfc43b
Glenn G. Chappell <ggchappell@alaska.edu>
2020-04-09 23:38:25 -0800

    Add interpit.lua (INCOMPLETE), degu.lua

---
+++ interpit.lua
@@ -0,0 +1,182 @@
+-- interpit.lua  INCOMPLETE
+-- Glenn G. Chappell
+-- 2020-04-09
+--
+-- For CS F331 / CSCE A331 Spring 2020
+-- Interpret AST from parseit.parse
+-- For Assignment 6, Exercise 1
+
+
+-- ***************************************************************
+-- * To run a Degu program, use degu.lua (which uses this file). *
+-- ***************************************************************
+
+
+local interpit = {}  -- Our module
+
+
+-- ***** Variables *****
+
+
+-- Symbolic Constants for AST
+
+local STMT_LIST    = 1
+local PRINT_STMT   = 2
+local FUNC_DEF     = 3
+local FUNC_CALL    = 4
+local IF_STMT      = 5
+local WHILE_STMT   = 6
+local RETURN_STMT  = 7
+local ASSN_STMT    = 8
+local STRLIT_OUT   = 9
+local CHAR_CALL    = 10
+local BIN_OP       = 11
+local UN_OP        = 12
+local NUMLIT_VAL   = 13
+local BOOLLIT_VAL  = 14
+local INPUT_CALL   = 15
+local SIMPLE_VAR   = 16
+local ARRAY_VAR    = 17
+
+
+-- ***** Utility Functions *****
+
+
+-- numToInt
+-- Given a number, return the number rounded toward zero.
+local function numToInt(n)
+    assert(type(n) == "number")
+
+    if n >= 0 then
+        return math.floor(n)
+    else
+        return math.ceil(n)
+    end
+end
+
+
+-- strToNum
+-- Given a string, attempt to interpret it as an integer. If this
+-- succeeds, return the integer. Otherwise, return 0.
+local function strToNum(s)
+    assert(type(s) == "string")
+
+    -- Try to do string -> number conversion; make protected call
+    -- (pcall), so we can handle errors.
+    local success, value = pcall(function() return tonumber(s) end)
+
+    -- Return integer value, or 0 on error.
+    if success then
+        if value == nil then
+            return 0
+        else
+            return numToInt(value)
+        end
+    else
+        return 0
+    end
+end
+
+
+-- numToStr
+-- Given a number, return its string form.
+local function numToStr(n)
+    assert(type(n) == "number")
+
+    return tostring(n)
+end
+
+
+-- boolToInt
+-- Given a boolean, return 1 if it is true, 0 if it is false.
+local function boolToInt(b)
+    assert(type(b) == "boolean")
+
+    if b then
+        return 1
+    else
+        return 0
+    end
+end
+
+
+-- astToStr
+-- Given an AST, produce a string holding the AST in (roughly) Lua form,
+-- with numbers replaced by names of symbolic constants used in parseit.
+-- A table is assumed to represent an array.
+-- See the Assignment 4 description for the AST Specification.
+--
+-- THIS FUNCTION IS INTENDED FOR USE IN DEBUGGING ONLY!
+-- IT SHOULD NOT BE CALLED IN THE FINAL VERSION OF THE CODE.
+function astToStr(x)
+    local symbolNames = {
+        "STMT_LIST", "PRINT_STMT", "FUNC_DEF", "FUNC_CALL", "IF_STMT",
+        "WHILE_STMT", "RETURN_STMT", "ASSN_STMT", "STRLIT_OUT",
+        "CHAR_CALL", "BIN_OP", "UN_OP", "NUMLIT_VAL", "BOOLLIT_VAL",
+        "INPUT_CALL", "SIMPLE_VAR", "ARRAY_VAR"
+    }
+    if type(x) == "number" then
+        local name = symbolNames[x]
+        if name == nil then
+            return "<Unknown numerical constant: "..x..">"
+        else
+            return name
+        end
+    elseif type(x) == "string" then
+        return '"'..x..'"'
+    elseif type(x) == "boolean" then
+        if x then
+            return "true"
+        else
+            return "false"
+        end
+    elseif type(x) == "table" then
+        local first = true
+        local result = "{"
+        for k = 1, #x do
+            if not first then
+                result = result .. ","
+            end
+            result = result .. astToStr(x[k])
+            first = false
+        end
+        result = result .. "}"
+        return result
+    elseif type(x) == "nil" then
+        return "nil"
+    else
+        return "<"..type(x)..">"
+    end
+end
+
+
+-- ***** Primary Function for Client Code *****
+
+
+-- interp
+-- Interpreter, given AST returned by parseit.parse.
+-- Parameters:
+--   ast     - AST constructed by parseit.parse
+--   state   - Table holding Degu variables & functions
+--             - AST for function xyz is in state.f["xyz"]
+--             - Value of simple variable xyz is in state.v["xyz"]
+--             - Value of array item xyz[42] is in state.a["xyz"][42]
+--   incall  - Function to call for line input
+--             - incall() inputs line, returns string with no newline
+--   outcall - Function to call for string output
+--             - outcall(str) outputs str with no added newline
+--             - To print a newline, do outcall("\n")
+-- Return Value:
+--   state, updated with changed variable values
+function interpit.interp(ast, state, incall, outcall)
+    print(astToStr(ast))
+    print("Function 'interp' needs to be written!!!")
+    return state
+end
+
+
+-- ***** Module Export *****
+
+
+return interpit
+
